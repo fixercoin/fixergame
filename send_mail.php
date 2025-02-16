f7242faecbfcd3f5f81137480fdf471efa850c81
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-16 19:26:35 +0500

    send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -9,9 +9,9 @@ $webmaster_email = "khanbabusargodhaa@gmail.com";
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
-$feedback_page = "feedback_form.html";
-$error_page = "error_message.html";
-$thankyou_page = "thank_you.html";
+$feedback_page = "https://tinyurl.com/superiorcollege-sgd";
+$error_page = "https://tinyurl.com/superiorcollege-sgd";
+$thankyou_page = "https://tinyurl.com/superiorcollege-sgd";
 
 /*
 This next bit loads the form field data into variables.
