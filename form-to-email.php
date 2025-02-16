621b979a164b7d5f15309455eddc41e9a31315af
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-16 17:34:57 +0500

    form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -3,3 +3,11 @@
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
 ?>
+<?php
+	$email_from = 'yourname@yourwebsite.com';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+?>
