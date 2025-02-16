5bd4f808f2f8947a425d4b81d9f6237b021bfb5c
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-16 17:35:44 +0500

    form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -11,3 +11,14 @@
 	$email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
 ?>
+<?php
+
+  $to = "yourname@yourwebsite.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
