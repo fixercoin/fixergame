d93e4c2a859b2d4c48cf1fde09d33189c0eb119e
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-16 17:37:16 +0500

    form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -27,3 +27,16 @@
 
   mail($to,$email_subject,$email_body,$headers);
 ?>
+<?php
+$to = "name1@website-name.com, name2@website-name.com,name3@website-name.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+$headers .= "Cc: someone@domain.com \r\n";
+
+$headers .= "Bcc: someoneelse@domain.com \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+?>
