57fb66803ecf12bd80ad8cb615d4563eb74c24eb
fixercoin <khanbabusargodhaa@gmail.com>
2025-02-23 18:13:56 +0500

    simpleBar.js

--- simpleBar.js
+++ simpleBar.js
@@ -118,9 +118,9 @@ function buildYAxis(chartData) {
     });
 
     //Setting Max Range here, will be used in height calculation of bar
-    maxRageYAxis = revArr[0] + 500;
-    var pointInterval = maxRageYAxis / 10;
-    var initialVal = revArr[0] + 500;
+    maxRageYAxis = revArr[0] + 50;
+    var pointInterval = maxRageYAxis / 1;
+    var initialVal = revArr[0] + 50;
     var dynamicYAxis = "";
 
     for (let k = 0; k < 10; k++) {
